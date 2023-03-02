4c4fdd59af88b696fdb3d815f8fb3bbe85b1243d
Akshay <aksrahangdale@gmail.com>
2023-03-02 14:32:18 +0530

    added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path =require('path');
 const mongoose = require("mongoose");
 // const cors=require("cors");
 const dotenv = require("dotenv");
@@ -10,7 +11,7 @@ mongoose.set('strictQuery', true);
 const transactions = require("./routes/transactions");
 
 try {
-  mongoose.connect(process.env.MONGO_URI);  
+  mongoose.connect("mongodb+srv://aks123:12@aks.4o89nep.mongodb.net/expense-tracker?retryWrites=true&w=majority");  
   console.log("DB Connected");
 } catch (err) {
   console.log(err);
@@ -18,9 +19,13 @@ try {
 
 // app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const YOUR_DOMAIN = 'http://localhost:5000'
+
 app.use("/api/v1/transactions", transactions);
+
+app.use(express.static('client/build'));
+app.get('*', (req,res) => res.sendFile(path.resolve(__dirname,'client','build','index.html')));
 
 app.listen(5000, () => {
   console.log("Backend Server running ");
